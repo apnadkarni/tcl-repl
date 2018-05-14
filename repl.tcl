db7bb77e91cd859b03d4910a8268696f323af112
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2018-05-14 06:53:16 +0530

    Added namespace completion. Made complete ensemble.

--- repl.tcl
+++ repl.tcl
@@ -9,7 +9,7 @@ package require Tcl 8.6
 
 namespace eval repl {}
 
-proc repl::command {prefix {ip {}} {ns ::}} {
+proc repl::complete_command {prefix {ip {}} {ns ::}} {
     # Finds all command names with the specified prefix
     #  prefix - a prefix to be matched with command names
     #  ip     - the interpreter whose context is to be used.
@@ -29,19 +29,16 @@ proc repl::command {prefix {ip {}} {ns ::}} {
     # Escape glob special characters in the prefix
     set esc_prefix [string map {* \\* ? \\? \\ \\\\} $prefix]
 
-    #ruff
-    # If the $ns is specified as the empty string, it defaults to the
-    # global namespace.
     if {$ns eq ""} {
         set ns ::
     }
 
     # Look for matches in the target context
-    set matches [_interp_eval $ip $ns ::info commands ${esc_prefix}*]
+    set matches [evaluate $ip $ns ::info commands ${esc_prefix}*]
     return [_return_matches $prefix $matches]
 }
 
-proc repl::variable {prefix {ip {}} {ns ::}} {
+proc repl::complete_variable {prefix {ip {}} {ns ::}} {
     # Finds all variable names with the specified prefix
     #  prefix - a prefix to be matched with variable names
     #  ip     - the interpreter whose context is to be used.
@@ -71,7 +68,7 @@ proc repl::variable {prefix {ip {}} {ns ::}} {
     if {[regexp {([^\(]*)\((.*)} $prefix -> arr elem_prefix]} {
         # Escape glob special characters
         set esc_elem [string map {* \\* ? \\? \\ \\\\} $elem_prefix]
-        set elems [_interp_eval $ip $ns ::array names $arr ${esc_elem}*]
+        set elems [evaluate $ip $ns ::array names $arr ${esc_elem}*]
         if {[llength $elems] == 1} {
 	    set var "$arr\([lindex $elems 0]\)"
             return [list $var [list $var]]
@@ -86,12 +83,43 @@ proc repl::variable {prefix {ip {}} {ns ::}} {
         return [list $prefix {}]
     } else {
         # Does not look like an array
-        set matches [_interp_eval $ip $ns ::info vars ${esc_prefix}*]
+        set matches [evaluate $ip $ns ::info vars ${esc_prefix}*]
         return [_return_matches $prefix $matches]
     }
 }
 
-proc repl::method {oo obj prefix {ip {}} {ns ::}} {
+proc repl::complete_namespace {prefix {ip {}} {ns ::}} {
+    # Finds all namespace names with the specified prefix
+    #  prefix - a prefix to be matched with the namespace names in target interpreter
+    #  ip     - the target interpreter whose context is to be used.
+    #           Defaults to current interpreter.
+    #  ns     - the namespace context for the command. Defaults to
+    #           the global namespace if unspecified or the empty string.
+    #
+    # The command looks for namespaces in the specified
+    # interpreter and namespace context that begin with $prefix.
+    #
+    # The return value is a pair consisting of the longest common
+    # prefix of all matching namespace names and a sorted list of all
+    # matching names.  If no namespaces match, the first element is
+    # the passed in prefix and the second element is an empty list.
+
+    # Escape glob special characters in the prefix
+    set esc_prefix [string map {* \\* ? \\? \\ \\\\} $prefix]
+
+    if {$ns eq ""} {
+        set ns ::
+    }
+
+    # Look for matches in the target context. 
+    set matches [lmap fqn [evaluate $ip :: ::namespace children $ns ${esc_prefix}*] {
+        namespace tail $fqn;    # We do not want fully qualified names
+    }]
+
+    return [_return_matches $prefix $matches]
+}
+
+proc repl::complete_method {oo obj prefix {ip {}} {ns ::}} {
     # Finds all method names with the specified prefix for a given TclOO object
     #  oo     - the OO subsystem, one of 'oo', 'nsf', 'xotcl'
     #  obj    - object name token
@@ -119,7 +147,7 @@ proc repl::method {oo obj prefix {ip {}} {ns ::}} {
     # a variable reference.
     if {[string index $obj 0] eq "\$"} {
         # Resolve the variable reference
-        set obj [_interp_eval $ip $ns set [string range $obj 1 end]]
+        set obj [evaluate $ip $ns set [string range $obj 1 end]]
     }
     
     # Escape glob special characters in the prefix
@@ -131,12 +159,12 @@ proc repl::method {oo obj prefix {ip {}} {ns ::}} {
             TBD
         }
         oo {
-            if {![_interp_eval $ip $ns ::info object isa object $obj]} {
+            if {![evaluate $ip $ns ::info object isa object $obj]} {
                 # Not an object.
                 return [list $prefix {}]
             }
 
-            set matches [lmap meth [_interp_eval $ip $ns ::info object methods $obj -all] {
+            set matches [lmap meth [evaluate $ip $ns ::info object methods $obj -all] {
                 if {![string match ${esc_prefix}* $meth]} continue
                 set meth
             }]
@@ -146,22 +174,22 @@ proc repl::method {oo obj prefix {ip {}} {ns ::}} {
         }
         nsf {
             # Next Scripting Framework
-            if {[_interp_eval $ip $ns ::nsf::object::exists $obj]} {
+            if {[evaluate $ip $ns ::nsf::object::exists $obj]} {
                 if {[string match ::* $prefix]} {
                     # NSF allows dispatch of unregistered methods via absolute paths
-                    set abs_matches [_interp_eval $ip $ns ::info commands ${esc_prefix}*]
-                    set ns_matches  [_interp_eval $ip $ns ::namespace children [namespace qualifiers ${esc_prefix}] ${esc_prefix}*]
+                    set abs_matches [evaluate $ip $ns ::info commands ${esc_prefix}*]
+                    set ns_matches  [evaluate $ip $ns ::namespace children [namespace qualifiers ${esc_prefix}] ${esc_prefix}*]
                     set matches [concat $abs_matches $ns_matches]
                 } else {
-                    set matches [_interp_eval $obj ::nsf::methods::object::info::lookupmethods -callprotection public -path -- ${esc_prefix}*]
+                    set matches [evaluate $obj ::nsf::methods::object::info::lookupmethods -callprotection public -path -- ${esc_prefix}*]
                 }
             } 
         }
         xotcl {
             # XOTcl
-            if {[_interp_eval $ip $ns ::info exists ::xotcl::version] &&
-                [_interp_eval $ip $ns ::xotcl::Object isobject $obj]} {
-                set matches [_interp_eval $ip $ns $obj info methods ${esc_prefix}*]
+            if {[evaluate $ip $ns ::info exists ::xotcl::version] &&
+                [evaluate $ip $ns ::xotcl::Object isobject $obj]} {
+                set matches [evaluate $ip $ns $obj info methods ${esc_prefix}*]
             }
         }
     }
@@ -184,6 +212,15 @@ proc repl::_return_matches {prefix matches} {
 }
 
 # Helper to evaluate commands in the target interpreter namespace
-proc repl::_interp_eval {ip ns args} {
+proc repl::evaluate {ip ns args} {
     return [interp eval $ip [list namespace eval $ns $args]]
+}
+
+namespace eval repl {
+    namespace ensemble create -command complete -map {
+        command complete_command
+        variable complete_variable
+        namespace complete_namespace
+        method complete_method
+    }
 }
